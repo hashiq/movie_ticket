2aade5ee4d2a18c91a2334fe359563a4ab2be6a4
luffy <hashiqhussain028@gmail.com>
2026-06-12 23:07:39 +0400

    committ

---
+++ movie_data.py
@@ -0,0 +1,464 @@
+movies = {
+    "shows" :{
+    "Mollywood Times": {
+        "details": {
+            "language": "Malayalam",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Superman": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Karuppu": {
+        "details": {
+            "language": "Tamil",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Drysham 3": {
+        "details": {
+            "language": "Malayalam",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "The Devil Wears Prada 2": {
+        "details": {
+            "language": "Malayalam",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "The Drama": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Peddi": {
+        "details": {
+            "language": "Telugu",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Blast": {
+        "details": {
+            "language": "Tamil",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Parimala and Co": {
+        "details": {
+            "language": "Tamil",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Star Wars: The Mandalorian and grogu": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Michael": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Obsession": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Masters of the Universe": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Supergirl": {
+        "details": {
+            "language": "Malayalam",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Welcome to the Jungle": {
+        "details": {
+            "language": "English",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+    "Toxic": {
+        "details": {
+            "language": "Kannada",
+            "duration_min": 156,
+            "rating": 8.3
+        },
+        "pricing": {
+            "standard": 8, "gold": 10,
+            "vip": 15
+        },
+        "schedule": {
+            "today": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            },
+            "tomorrow": {
+                "12:00 PM": {"seats": 20, "screen": 1},
+                "4:00 PM": {"seats": 15, "screen": 2},
+                "8:00 PM": {"seats": 25, "screen": 3}
+            }
+        },
+        "revenue": {
+            "tickets": 0,
+            "food": 0,
+            "drinks": 0
+        }
+    },
+},
+    "snacks_food":{
+        "food_menu": {
+            "popcorn": {"small": 3, "large": 5},
+            "nachos": 6,
+            "fries": 4
+        },
+        "drinks": {
+            "coke": 2,
+            "pepsi": 2,
+            "water": 1
+        },
+    }
+}
