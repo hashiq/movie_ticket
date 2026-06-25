346b678ad988b39f1300c2475c907b9c75f96c94
netsoft_pc <hashiqhussain028@gmail.com>
2026-06-25 15:36:43 +0400

    commit

---
+++ movie.py
@@ -0,0 +1,14 @@
+vox = {
+    "Athiradi":{
+        "Language":{"Malayalam","Gener","Comedy"},
+        "Running-time":120,
+        "Ticket":{"Seat":12,"Price":100},
+        "Revenue":{"profit":0}
+    },
+    "Karuppu": {
+        "Language": {"Malayalam", "Gener", "Comedy"},
+        "Running-time": 100,
+        "Ticket": {"Seat": 32, "Price": 120},
+        "Revenue": {"profit": 0}
+    }
+}
